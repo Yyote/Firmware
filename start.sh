984eb81bbc624013794494a0b09348951fc3b535
Leekai <leekai.dev@gmail.com>
2022-08-02 14:11:02 +0300

    add start script

---
+++ start.sh
@@ -0,0 +1,3 @@
+source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/px4_sitl_default
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
